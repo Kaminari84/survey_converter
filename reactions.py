f0af487770dbdb3ef50dbd56345e3f08afeca8bf
Kaminari84 <rafal.kocielnik@gmail.com>
2019-12-18 00:53:12 -0800

    Repository of reactions

---
+++ reactions.py
@@ -0,0 +1,97 @@
+reaction_repo = {
+    "intro_list": [
+        "Hi, my name is {name}. I would like to talk to you about {topic}.",
+        "Hi, I am {name}. Let's talk a bit about {topic}.",
+        "Hi, I am {name}. I'd like to ask you a few questions about {topic}.",
+        "Hi, my name is {name}. Let's start by talking about {topic}."
+    ],
+    "close_list": [
+        "We are done! Thanks.", 
+        "We are done! Thanks for your patience.",
+        "This is it! Thank you for your time.",
+        "We've completed the whole survey! Thanks a lot!"
+    ],
+    "progress_list": [
+        "We are currently at question {d} out of {n}.",
+        "We are done with {d} questions, still {l} to go.",
+        "We have completed {percent}% of the survey.",
+        "We are done with {percent}% of our questions."
+    ],
+    "progress_middle_list": [
+        "We are now in the middle of the survey",
+        "We are about half way through",
+        "Half of the survey is done, thanks for you patience"
+    ],
+    "progress_end_list": [
+        "We are almost done, thanks for your patience",
+        "We are almost at the end, thank you for staying that long",
+        "We are almost at the end of the survey. I appreciate your patience."
+    ],
+    "reaction_positive_list": [
+        "I am glad to hear that",
+        "I am happy this is not a problem",
+        "That's good",
+        "Sounds good",
+        "Sounds nice",
+        "Okay, that's good",
+        "Great!"
+    ],
+    "reaction_negative_list": [
+        "That is frustrating",
+        "I am sorry to hear that",
+        "So sorry about that",
+        "Thanks for sharing that",
+        "That sounds stressful"
+    ],
+    "reaction_neutral_list": [
+        "Got it",
+        "Thanks for sharing",
+        "Got it! Thanks for sharing",
+        "Sure",
+        "Noted",
+        "Thanks for letting me know",
+        "Okay, I'm getting a better idea of your answers",
+        "Thank you for your answer"
+    ],
+    #If I - pronoun in sent - problem - can't use
+    #If q starts with verb - rem verb
+    "q_prefix_start_list": [
+        "Can you tell me",
+        "Could you tell me",
+        "Would you mind sharing",
+        "Would you mind telling me",
+        "Can I ask you"
+    ],
+    "q_prefix_list": [
+        "Can you tell me",
+        "Could you tell me",
+        "Would you mind sharing",
+        "Would you mind telling me",
+        "Can I ask you",
+        "Next, can you tell me",
+        "Next, would you mind sharing",
+        "Next, can I ask you",
+        "Next, let me ask you",
+        "Going forward, could you tell me",
+        "Going forward, would you mind sharing",
+        "Going forward, can I ask you",
+        "Further, could you tell me",
+        "Further, let me ask you"
+    ],
+    "section_transitions": [
+        "Next, I want to talk about",
+        "Thank you fo your answers, let's move on",
+        "This is great, let's move on",
+        "Thanks for all the answers so far"
+    ]
+}
+
+#pos_answers = ["Definitely agree", "Agree with reservation", 
+#                "Extremely much", "Very much", "Yes", "Quite a bit", "Extremely",
+#                "Very High","Nearly every day","More than half of the days","Several days"]
+
+#neg_answers = ["Disagree with reservation", "Definitely disagree", 
+#                "Not much", "No", "Very slightly or not at all", "A little", 
+#                "Very Low", "Not at all"]
+
+#neu_answers = ["Moderately"]
